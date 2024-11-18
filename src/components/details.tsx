6cb040438350378db12d3826698f7ef04c5afe5c
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-11-18 08:03:19 +0530

    Update details.tsx

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -2,7 +2,7 @@ import movieRecomend from "./video/movieRecomend.gif";
 
 const arr = [
   {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/AllPost.png", "desc": "Blog and Earn","add":"https://blog-and-earn-frontend.vercel.app" },
-  {  "img": "", "desc": "Career Wave","add":"https://github.com/st321123/Course-selling-" },
+  {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/image.png", "desc": "Career Wave","add":"https://career-wave-two.vercel.app" },
  { "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/Weather.png", "link": "23","desc": "Weather Report","add":"https://weather-app-api-ten.vercel.app/" },
  { "img": movieRecomend, "link": "23", "desc": "Movie Recommendation","add":"https://github.com/st321123/Movie" },
  { "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/Keeper.png", "link": "23", "desc": "Keeper Web App","add":"https://new-keeper-app-nfo4.vercel.app/" },
