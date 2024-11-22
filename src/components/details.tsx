54e6cd30f35fd808189dbf95d737f8c3b78e8959
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-11-23 00:03:51 +0530

    Update details.tsx

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -3,7 +3,7 @@ import movieRecomend from "./video/movieRecomend.gif";
 const arr = [
   {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/AllPost.png", "desc": "Blog and Earn","add":"https://blog-and-earn-frontend.vercel.app" },
   {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/carrer%20wave.png", "desc": "Career Wave","add":"https://career-wave-two.vercel.app" },
-   {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/image.png", "desc": "Career Wave","add":"https://statistics-leetcodee.vercel.app" },
+   {  "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/image.png", "desc": "Code Miner","add":"https://statistics-leetcodee.vercel.app" },
  { "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/Weather.png", "link": "23","desc": "Weather Report","add":"https://weather-app-api-ten.vercel.app/" },
  { "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/Keeper.png", "link": "23", "desc": "Keeper Web App","add":"https://new-keeper-app-nfo4.vercel.app/" },
  { "img": "https://raw.githubusercontent.com/st321123/portfolio-data/refs/heads/main/Todo.png", "link": "23", "desc": "Todo-List","add":"https://to-do-list-kohl-eight.vercel.app/" }];
